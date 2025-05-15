c731720097eb3c214639c6ed7658a024bacde038
trh4ckn0n <trhacknon@proton.me>
2025-05-16 01:21:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("assets/styles.css")
+local_css("assets/style.css")
 
 # --- LOAD DATA ---
 def load_products():
